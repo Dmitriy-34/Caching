e0124c87bfcb4c9cfbaa27a69d81b2b3dbe1cd5b
Borisov_Dmitriy <92953878+Dmitriy-34@users.noreply.github.com>
2023-06-28 02:01:16 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,29 @@
+import redis
+import json
+
+red = redis.Redis(
+    host='redis-10120.c1.asia-northeast1-1.gce.cloud.redislabs.com',
+    port=10120,
+    password='lo7x4zi6MQPXPaAQXbAAnJfXX44RqK2F'
+)
+
+# Записываем данные в кеш
+
+red.set('var1', 'value1')  # записываем в кеш строку "value1"
+print(red.get('var1'))  # считываем из кеша данные
+
+# Добавляем словарь
+
+dict1 = {'key1': 'value1', 'key2': 'value2'} # создаём словарь для записи
+red.set('dict1', json.dumps(dict1)) # с помощью функции dumps() из модуля json превратим наш словарь в строчку
+
+converted_dict = json.loads(red.get('dict1')) # с помощью знакомой нам функции превращаем данные,
+# полученные из кеша обратно в словарь
+
+print(type(converted_dict)) # убеждаемся, что мы получили действительно словарь
+print(converted_dict) # ну и выводим его содержание
+
+# Удаляем данные из кеша по ключу
+
+red.delete('dict1')  # удаляются ключи с помощью метода .delete()
+print(red.get('dict1'))
